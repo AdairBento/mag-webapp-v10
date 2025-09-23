a0e77f406de4498262fb0d99ed9dedad606fa96d
Adair Bento <SEU_EMAIL_REAL_AQUI>
2025-09-23 13:09:36 -0300

    fix(api): inferir tipo do enqueueNotification a partir do delegate do prisma

--- api/src/services/notificationService.ts
+++ api/src/services/notificationService.ts
@@ -1,8 +1,11 @@
 import { prisma } from "../services/prisma";
 import { Prisma } from "@prisma/client";
 
-export async function enqueueNotification(
-  input: Parameters<typeof prisma.notificationMessage.create>[0]["data"],
-) {
+type NotificationMessageCreateInput = Parameters<
+  typeof prisma.notificationMessage.create
+>[0] extends { data: infer D }
+  ? D
+  : never;
+export async function enqueueNotification(input: NotificationMessageCreateInput) {
   return prisma.notificationMessage.create({ data: input });
 }
